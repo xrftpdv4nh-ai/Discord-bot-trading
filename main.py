a0bfc184b1f2b87afba4b741fb9e69660a5f76c4
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-02 21:33:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ import discord
 from discord.ext import commands
 
 from config import BOT_TOKEN
-
-# نستورد أمر واحد بس بسيط
 from commands.ping import ping
 from commands.embed import embed
 
 intents = discord.Intents.default()
-
 bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
     print("🟢 Bot Online")
 
-    # نمسح أي أوامر قديمة كانت متسجلة
     bot.tree.clear_commands(guild=None)
 
-    # نسجل أمر واحد فقط
     bot.tree.add_command(ping)
-bot.tree.add_command(embed)
+    bot.tree.add_command(embed)
+
     await bot.tree.sync()
+
     print("✅ Commands Synced")
+
 
 bot.run(BOT_TOKEN)
