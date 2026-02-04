91383633b088b5e6758dbe2c4653fdfc1d235dd0
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-04 16:47:33 +0200

    Update wallet_admin.py

--- admin/wallet_admin.py
+++ admin/wallet_admin.py
@@ -5,23 +5,49 @@ WALLET_FILE = "data/wallets.json"
 
 def add_balance(user_id: int, amount: int):
     """
-    إضافة رصيد للمستخدم
+    إضافة رصيد (نقاط) للمستخدم
     """
     wallets = load_json(WALLET_FILE, {})
-
     uid = str(user_id)
+
     if uid not in wallets:
         wallets[uid] = {
             "balance": 0,
-            "total_deposit": 0
+            "total_earned": 0
         }
 
-    wallets[uid]["balance"] += amount
-    wallets[uid]["total_deposit"] += amount
+    wallets[uid]["balance"] += int(amount)
+    wallets[uid]["total_earned"] += int(amount)
 
     save_json(WALLET_FILE, wallets)
 
 
-# لو عندك أوامر أدمن نصية سيبها زي ما هي
-async def handle_admin_message(bot, message):
-    pass
+def remove_balance(user_id: int, amount: int):
+    """
+    خصم رصيد من المستخدم
+    """
+    wallets = load_json(WALLET_FILE, {})
+    uid = str(user_id)
+
+    if uid not in wallets:
+        return False
+
+    if wallets[uid]["balance"] < amount:
+        return False
+
+    wallets[uid]["balance"] -= int(amount)
+    save_json(WALLET_FILE, wallets)
+    return True
+
+
+def get_balance(user_id: int):
+    """
+    جلب رصيد المستخدم
+    """
+    wallets = load_json(WALLET_FILE, {})
+    uid = str(user_id)
+
+    if uid not in wallets:
+        return 0
+
+    return wallets[uid].get("balance", 0)
